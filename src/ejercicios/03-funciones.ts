acebb063fb10a848dcc09ef181cd948546523838
Inma Gijón <igijoninf@gmail.com>
2022-05-24 18:23:33 +0200

    Funciones básicas

---
+++ src/ejercicios/03-funciones.ts
@@ -0,0 +1,45 @@
+/*
+    ===== Código de TypeScript =====
+*/
+function sumar(a: number,b: number): number {
+    return a+b;
+}
+
+const sumarFlecha = (a: number, b: number): number => {
+    return a+b;
+}
+
+/**Primero van los parámetros obligatorios, luego los opcionales y luego los que tienen un valor por defecto */
+function multiplicar( numero: number, otroNumero?: number, base: number = 2): number {
+    return numero * base;
+}
+
+/*let resultado = multiplicar(10,20);
+resultado = multiplicar(5, 0 , 10)
+console.log(resultado);
+*/
+
+interface PersonajeLOR {
+    nombre: string;
+    pv: number;
+    mostrarHp: () => void;
+
+}
+
+function curar(personaje: PersonajeLOR, curarX: number): void {
+
+    personaje.pv += curarX;
+
+}
+
+const nuevoPersonaje: PersonajeLOR = {
+    nombre: 'Strider',
+    pv: 50,
+    mostrarHp() {
+        console.log('Puntos de vida: ', this.pv);
+        
+    }
+}
+
+curar ( nuevoPersonaje, 20 );
+nuevoPersonaje.mostrarHp();
