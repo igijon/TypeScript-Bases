3566ab8f08631cafb6d2401eae3e6edd915b9e91
Inma Gijón <igijoninf@gmail.com>
2022-05-25 12:38:50 +0200

    Clases y herencia

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,3 @@
     ===== Código de TypeScript =====
 */
 
-class Heroe {
-    // alterEgo: string;
-    // edad: number;
-    // nombreReal: number;
-
-    //Con una sola línea de código se crea la propiedad y se le asigna el valor que recibe
-    //por parámetros
-    constructor(
-        public alterEgo?: string, 
-        public edad?: number, 
-        public nombreReal?: string
-    ) {}
-}
-
-
-
-const ironman = new Heroe('Ironman', 45, 'Tony');
-
-console.log(ironman);
