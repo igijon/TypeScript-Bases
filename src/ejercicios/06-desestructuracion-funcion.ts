7b2530a7651cd57ed2e24d2c1d7c1310b65a339f
Inma Gijón <igijoninf@gmail.com>
2022-05-25 11:47:24 +0200

    Imports y exports

--- src/ejercicios/06-desestructuracion-funcion.ts
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -2,7 +2,9 @@
     ===== Código de TypeScript =====
 */
 
-interface Producto {
+//Este archivo va a poner a disposición de todo el mundo que se puede consumir
+//esta interfaz. Para ello habrá que importarla
+export interface Producto {
     desc: string;
     precio: number;
 }
@@ -18,7 +20,7 @@ const tableta: Producto = {
     precio: 350
 } 
 
-function calculaISV( productos: Producto[] ): [number, number] {
+export function calculaISV( productos: Producto[] ): [number, number] {
 
     let total = 0;
 
@@ -33,8 +35,8 @@ function calculaISV( productos: Producto[] ): [number, number] {
 
 }
 
-const articulos = [ telefono, tableta ];
-const [total,isv] = calculaISV( articulos );
+// const articulos = [ telefono, tableta ];
+// const [total,isv] = calculaISV( articulos );
 
-console.log('Total: ', total);
-console.log('ISV: ', isv);
+// console.log('Total: ', total);
+// console.log('ISV: ', isv);
