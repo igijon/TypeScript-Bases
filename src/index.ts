89eec5fe17d69c4d4a278f8127339d62727eb35c
Inma Gijón <igijoninf@gmail.com>
2022-05-25 11:55:34 +0200

    Clases e interfaces básicas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 /*
     ===== Código de TypeScript =====
 */
+
+class Heroe {
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
+
+    imprimirNombre() {
+        return this.alterEgo+' '+this.nombreReal;
+    }
+
+}
+
+interface Personaje2 {
+    alterEgo?: string;
+    edad?: number;
+    nombreReal?: number;
+
+    imprimirNombre?: () => string; //Pero no puedo implementarlo aquí
+}
+
+const ironman = new Heroe();
+const spiderman: Personaje2 = {}
+
+console.log(ironman);
